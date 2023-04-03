0cfd40202f45652c7b795fd67c001da7e02af7e2
Niels <niels.vandeginste@student.ap.be>
2023-04-03 11:54:58 +0200

    CSS toevoegen voor de leut

--- generate.py
+++ generate.py
@@ -21,14 +21,27 @@ def generatePageHtml():
     pageContent, pageMetaData = convert.getPagesData()
     for i in range(0,len(pageContent)):
         match pageMetaData[i]["filename"]:
-            case "nav":
-                postContent, postMetaData = convert.getPostData()
+            case "main":
+                postMetaData = convert.getPostData()
                 env = jinja2.Environment(loader=jinja2.FileSystemLoader("templates"))
-                template= env.get_template("navTemplate.html")
+                template= env.get_template("mainTemplate.html")
                 renderedPage = template.render(
-                    posts=postMetaData,
-                    title="Navigatiepagina",
+                    pages=pageMetaData,
+                    posts=postMetaData[1],
+                    title=pageMetaData[i]["title"],
                     content=pageContent[i]
                 )
                 with open(f"_site/index.html","w") as htmlPost:
+                    htmlPost.write(renderedPage)
+                    
+            case _:
+                env = jinja2.Environment(loader=jinja2.FileSystemLoader("templates"))
+                template= env.get_template("defaultTemplate.html")
+                renderedPage = template.render(
+                    title=pageMetaData[i]["title"],
+                    content=pageContent[i]
+                )
+                destFolder = pathlib.Path(f"_site/pages/{pageMetaData[i]['filename']}")
+                destFolder.mkdir(parents=True,exist_ok=True)
+                with open(f"{destFolder}/index.html","w") as htmlPost:
                     htmlPost.write(renderedPage)
